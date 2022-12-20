6e46eeb91ba5dd7e47bfbdfe035a080870c45a1f
KolciFranceska <195387@ecam.be>
2022-12-20 23:28:27 +0100

    change port

--- server.js
+++ server.js
@@ -72,6 +72,6 @@ app.get('/delete/:i', (req, res) => {
 });
 
 //Launch app to listen to specified port
-app.listen(8000, function () {
-    console.log("Running on port 8000");
+app.listen(80, function () {
+    console.log("Running on port 80");
 });
